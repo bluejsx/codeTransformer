0e9091dc6bf2d355d1bcc3478a360d98f78844dc
Momijiichigo <ichigomomiji436@gmail.com>
2022-05-28 17:30:36 -0700

    package files

---
+++ scripts/build_npm.ts
@@ -0,0 +1,29 @@
+import { build, emptyDir } from "https://deno.land/x/dnt/mod.ts";
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./src/index.ts"],
+  outDir: "./npm",
+  shims: {
+    // see JS docs for overview and more options
+    deno: true,
+  },
+  package: {
+    // package.json properties
+    name: "bluejsx/code-transformer",
+    version: Deno.args[0],
+    description: "TS/JS code transformer made for BlueJSX plugins",
+    license: "MIT",
+    repository: {
+      type: "git",
+      url: "git@github.com:bluejsx/codeTransformer.git",
+    },
+    bugs: {
+      url: "https://github.com/bluejsx/codeTransformer/issues",
+    },
+  },
+});
+
+// post build steps
+Deno.copyFileSync("README.md", "npm/README.md");
